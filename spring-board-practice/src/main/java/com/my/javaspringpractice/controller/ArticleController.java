1a501d903415aff460da6c1fd1bc4a2cd74d4261
nomoreFt <hyunwoo.kim1108@gmail.com>
2023-10-21 00:19:10 +0900

    feat : 게시글 관련 뷰 기본 골격 작성#12
    * /articles 테스트를 열어주고 기본 html 뼈대 추가
    
    * 테스트에서 media-Type 호환 타입이면 모두 가능하게 수정

--- spring-board-practice/src/main/java/com/my/javaspringpractice/controller/ArticleController.java
+++ spring-board-practice/src/main/java/com/my/javaspringpractice/controller/ArticleController.java
@@ -1,11 +1,19 @@
 package com.my.javaspringpractice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map){
+        map.addAttribute("articles", "articles");
+        return "articles/index";
+    }
 
 
 }
