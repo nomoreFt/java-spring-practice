03571c945b9228b7aa237fdf3ddef65a1d4e4160
nomoreFt <hyunwoo.kim1108@gmail.com>
2023-10-28 16:06:45 +0900

    feat : 게시판 페이징 구현 #15
    댓글 Service 구현

--- spring-board-practice/src/main/java/com/my/javaspringpractice/service/ArticleCommentService.java
+++ spring-board-practice/src/main/java/com/my/javaspringpractice/service/ArticleCommentService.java
@@ -5,11 +5,13 @@ import com.my.javaspringpractice.dto.ArticleCommentDto;
 import com.my.javaspringpractice.repository.ArticleCommentRepository;
 import com.my.javaspringpractice.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -27,14 +29,24 @@ public class ArticleCommentService {
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+        } catch (Exception e) {
+            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
 
     }
 
     public void updateArticleComment(ArticleCommentDto dto) {
-
+        try {
+            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
+            if (dto.content() != null) articleComment.setContent(dto.content());
+        } catch (Exception e) {
+            log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     public void deleteArticleComment(Long articleCommentId) {
-
+        articleCommentRepository.deleteById(articleCommentId);
     }
 }
