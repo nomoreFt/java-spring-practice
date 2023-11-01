cbff8424c929ac9521376a744cbfbfa1809eda8a
nomoreFt <hyunwoo.kim1108@gmail.com>
2023-11-01 18:03:59 +0900

    feat : JPA Auditing config 추가

--- spring-board-practice/src/main/java/com/my/javaspringpractice/domain/BaseEntity.java
+++ spring-board-practice/src/main/java/com/my/javaspringpractice/domain/BaseEntity.java
@@ -5,8 +5,10 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -22,13 +24,15 @@ public abstract class BaseEntity {
     @Column(nullable = false, updatable = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime createdAt;
 
+    @CreatedBy
     @Column(nullable = false, updatable = false, length = 100)
     private String createdBy;
 
-    @LastModifiedBy
+    @LastModifiedDate
     @Column(nullable = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime modifiedAt;
 
+    @LastModifiedBy
     @Column(nullable = false, length = 100)
     private String modifiedBy;
 }
